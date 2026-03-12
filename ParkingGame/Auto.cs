c55bc2885cd100448701647e0a4992272e63ea13
01Master00 <berkibalintka@gmail.com>
2026-03-12 11:16:32 +0100

    Hozzáadtam egy alapszintű autó class-t

---
+++ ParkingGame/Auto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ParkingGame
+{
+    internal class Auto
+    {
+        Button Button;
+        int X;
+        int Y;
+        int Direction;
+
+        public Auto(Button b, int x, int y, int direction)
+        {
+            Button = b;
+            X = x;
+            Grid.SetRow(Button, x);
+            Y = y;
+            Grid.SetColumn(Button, y);
+            Direction = direction;
+        }
+
+        public void RemoveCar()
+        {
+            Button.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        public Button button { get => Button; set => Button = value; }
+        public int x1 { get => X; set => X = value; }
+        public int y1 { get => Y; set => Y = value; }
+        public int Direction1 { get => Direction; }
+    }
+}
--- ParkingGame/GameWindow.xaml.cs
+++ ParkingGame/GameWindow.xaml.cs
@@ -66,13 +66,13 @@ namespace ParkingGame
 
 		private void Gwidth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
-			GlabelWidth.Content = "Adja meg a pálya szélességét ami jelenleg " + Math.Floor(Gwidth.Value);
+			GlabelWidth.Content = "Adja meg a pálya magasságát ami jelenleg " + Math.Floor(Gwidth.Value);
 
 		}
 
 		private void Gheight_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
-			GlabelHeight.Content = "Adja meg a pálya magasságát ami jelenleg " + Math.Floor(Gheight.Value);
+			GlabelHeight.Content = "Adja meg a pálya szélességét ami jelenleg " + Math.Floor(Gheight.Value);
 
 		}
 	}
