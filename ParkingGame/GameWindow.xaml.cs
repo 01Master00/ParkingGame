8794f0ad309df00e1a552d31adef7c56a15a80c2
Petrik Diak <email@petrik.hu>
2026-03-18 10:02:08 +0100

    játék rendszer átalakítás, felkészülés a szintekre

--- ParkingGame/GameWindow.xaml.cs
+++ ParkingGame/GameWindow.xaml.cs
@@ -20,35 +20,29 @@ namespace ParkingGame
 	/// </summary>
 	public partial class GameWindow : Window
 	{
+        private GameArea ga;
 		public GameWindow()
 		{
 			InitializeComponent();
 
 		}
-
-        private double cellWidth; //univerzális változók, ez alapján látom hol vannak a cellák, és hogy mekkora egy cella. Hangsúlyban a HOL
-        private double cellHeight;
 		private void CanvaFelosztas()
 		{
-            double width = Game.ActualWidth;
-            double height = Game.ActualHeight;
-            cellWidth = width / Gwidth.Value;
-            cellHeight = height / Gheight.Value;
             // ide jöhet majd az autók generálása
-            for (int i = 0; i < Gwidth.Value; i++)
+            for (int i = 0; i < ga.width; i++)
             {
-                for (int j = 0; j < Gheight.Value; j++)
+                for (int j = 0; j < ga.height; j++)
                 {
 					
                     Rectangle rect = new Rectangle
                     {
-                        Width = cellWidth,
-                        Height = cellHeight,
+                        Width = ga.widthFeloszt,
+                        Height = ga.heightFeloszt,
                         Stroke = Brushes.Black,
                         Fill = Brushes.LightGray
                     };
-                    Canvas.SetLeft(rect, i * cellWidth);  //FONTOS:   LEFT - TOP.  ezeket használjuk mérőezköznek
-                    Canvas.SetTop(rect, j * cellHeight);
+                    Canvas.SetLeft(rect, i * ga.widthFeloszt);  //FONTOS:   LEFT - TOP.  ezeket használjuk mérőezköznek
+                    Canvas.SetTop(rect, j * ga.heightFeloszt);
                     Game.Children.Add(rect);
                 }
             }
@@ -65,7 +59,7 @@ namespace ParkingGame
 
         public bool Check(Cord c) //ha nem szabad helyre néz akkor false, ha szabad akkor true
         {
-            if (c.x < 0 || c.y < 0 || c.x >= Gwidth.Value || c.y >= Gheight.Value)
+            if (c.x < 0 || c.y < 0 || c.x >= ga.width || c.y >= ga.height)
             {
                 return false; //pályán kívül van
             }
@@ -73,8 +67,8 @@ namespace ParkingGame
             {
                 double l = Canvas.GetLeft(child);
                 double t = Canvas.GetTop(child);
-                int x = (int)(l / cellWidth);
-                int y = (int)(t / cellHeight);
+                int x = (int)(l / ga.widthFeloszt);
+                int y = (int)(t / ga.heightFeloszt);
                 if (x == c.x && y == c.y)
                 {
                     return false;
@@ -82,19 +76,6 @@ namespace ParkingGame
             }
             return true;
         }
-
-
-        private void Gwidth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-		{
-			GlabelWidth.Content = "Magasság, jelenleg: " + Math.Floor(Gwidth.Value);
-
-		}
-
-		private void Gheight_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-		{
-			GlabelHeight.Content = "Hosszúság jelenleg: " + Math.Floor(Gheight.Value);
-
-		}
 
         private void Back_Click(object sender, RoutedEventArgs e)
         {
