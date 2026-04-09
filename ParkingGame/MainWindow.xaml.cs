010d9fb477194f3406e18bbbc9c062056e2004d8
farkasRobi <farkas.robert2007@gmail.com>
2026-04-09 19:54:09 +0200

    credits final (maybe)

--- ParkingGame/MainWindow.xaml.cs
+++ ParkingGame/MainWindow.xaml.cs
@@ -7,9 +7,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Markup;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ParkingGame
 {
@@ -26,10 +28,10 @@ namespace ParkingGame
 
         }
 
-        private void Credits_Click(object sender, RoutedEventArgs e)
+        private DispatcherTimer gameTimer;
+        private void CreditsCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            Credits.Visibility = Visibility.Visible;
-
+            StartCreditsScroll();
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
@@ -37,18 +39,81 @@ namespace ParkingGame
             Close();
         }
 
-		private void PlayClick(object sender, RoutedEventArgs e)
-		{
+        private void PlayClick(object sender, RoutedEventArgs e)
+        {
             GameWindow GW = new GameWindow();
             GW.Show();
-            
-		}
 
-		private void BackButton_Click(object sender, RoutedEventArgs e)
-		{
-			Credits.Visibility = Visibility.Collapsed;
-		}
+        }
 
+        private void CreditsCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void Credits_Click(object sender, RoutedEventArgs e)
+        {
+            Credits.Visibility = Visibility.Visible;
+            StartCreditsScroll();
+            StartDvdAnimation();
+        }
+
+        private void StartCreditsScroll()
+        {
+            CreditsList.UpdateLayout();
+            var transform = new TranslateTransform();
+            CreditsList.RenderTransform = transform;
+
+            double canvasHeight = CreditsCanvas.ActualHeight;
+            double listHeight = CreditsList.ActualHeight;
+
+            var animation = new DoubleAnimation
+            {
+                From = -listHeight,
+                To = canvasHeight,
+                Duration = TimeSpan.FromSeconds(4),
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            transform.BeginAnimation(TranslateTransform.YProperty, animation);
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Credits.Visibility = Visibility.Collapsed;
+            if (gameTimer != null)
+                gameTimer.Stop();
+            if (dvdTimer != null)
+                dvdTimer.Stop();
+        }
+        DispatcherTimer dvdTimer;
+        double dx = 3;
+        double dy = 3;
+
+        private void StartDvdAnimation()
+        {
+            Canvas.SetLeft(DvdText, 50);
+            Canvas.SetTop(DvdText, 50);
+
+            dvdTimer = new DispatcherTimer();
+            dvdTimer.Interval = TimeSpan.FromMilliseconds(16);
+            dvdTimer.Tick += DvdTimer_Tick;
+            dvdTimer.Start();
+        }
+        private void DvdTimer_Tick(object sender, EventArgs e)
+        {
+            double x = Canvas.GetLeft(DvdText);
+            double y = Canvas.GetTop(DvdText);
+            x += dx;
+            y += dy;
+            if (x <= 0 || x + DvdText.ActualWidth >= DvdCanvas.ActualWidth)
+                dx = -dx;
+
+            if (y <= 0 || y + DvdText.ActualHeight >= DvdCanvas.ActualHeight)
+                dy = -dy;
+
+            Canvas.SetLeft(DvdText, x);
+            Canvas.SetTop(DvdText, y);
+        }
     }
 }
