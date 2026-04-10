02d68da1c9bf3a5bde0accde084a77af1b5ff124
01Master00 <berkibalintka@gmail.com>
2026-04-10 13:51:11 +0200

    Cleaned up the code, got rid of useless parts

--- ParkingGame/Auto.cs
+++ ParkingGame/Auto.cs
@@ -42,96 +42,27 @@ namespace ParkingGame
 
         }
 
-
+        /*
         //csekkolja a valid autolpacementet null ha jó, ha nem akkor visszaadja a problémás koordinátát
         public Cord CheckSurroundings(GameWindow gw)
         {
-            Cord CheckCord;
             int dir = this.direction;
-
-            //orientálódás
-            if (this.headC.y == this.tailC.y && this.headC.x == this.tailC.x) // nem valid autó, killswich
+            List<Cord> SurroundingCords = this.headC.GetSurroundingCords(gw);
+            foreach(Cord c in this.tailC.GetSurroundingCords(gw))
             {
-                MessageBox.Show("Nem valid autó, a két koordináta nem egy vonalban van!");
-                return null;
-            }
-            switch (dir)
-            {
-                case 0: 
-                    CheckCord = new Cord(this.headC.x, this.headC.y + 1);  
-                    break;
-                case 1: 
-                    CheckCord = new Cord(this.headC.x + 1, this.headC.y); 
-                    break;
-                case 2:
-                    CheckCord = new Cord(this.tailC.x, this.tailC.y + 1);
-                    break;
-                case 3:
-                    CheckCord = new Cord(this.tailC.x + 1, this.tailC.y); 
-                    break;
-                default:  //csak 4 irány van, ha ez nem stimmel akkor valami nagyon rossz van
-                    MessageBox.Show("Nem valid irány");
-                    return null;
+                SurroundingCords.Add(c);
             }
 
-            for (int i = 0; i < 5; i++) //i: (-1)-irány és óra járásnak megfelelően halad
+            if (SurroundingCords == null) return null; //ha nincs szabad hely a környéken akkor jó
+
+            foreach (Cord c in SurroundingCords)
             {
-                //if (CheckCord.DeepCheck(gw) != null) return CheckCord.DeepCheck(gw); //megnézi hogy van-e hely minden kocsinak a környéken
-                if (dir % 2 == 0) // vertikális autó
-                {
-                    switch (i) // Checkcords-on relatív kordináták segítségével végiglépkedek, és megnézem hogy van-e ott autó, vagy pályán kívül van-e
-                    {
-                        case 0:
-                            CheckCord.x++;
-                            CheckCord.y--;
-                            break;
-                        case 1:
-                            CheckCord.y--;
-                            break;
-                        case 2:
-                            CheckCord.x--;
-                            CheckCord.y--;
-                            break;
-                        case 3:
-                            CheckCord.x--;
-                            CheckCord.y++;
-                            break;
-                        case 4:
-                            CheckCord.y++;
-                            break;
-                        default:
-                            return null;
-                    }
-                }
-                else // horizontális autó
-                {
-                    switch (i) // Checkcords-on végiglépkedek, és megnézem hogy van-e ott autó, vagy pályán kívül van-e
-                    {
-                        case 0:
-                            CheckCord.x--;
-                            CheckCord.y--;
-                            break;
-                        case 1:
-                            CheckCord.x--;
-                            break;
-                        case 2:
-                            CheckCord.x--;
-                            CheckCord.y++;
-                            break;
-                        case 3:
-                            CheckCord.x++;
-                            CheckCord.y++;
-                            break;
-                        case 4:
-                            CheckCord.x++;
-                            break;
-                        default:
-                            return null;
-                    }
-                }
+                Cord dc = c.DeepCheck(gw);
+                if (dc != null) return dc; //megnézi hogy van-e hely minden kocsinak a környéken
             }
             return null;
 
         }
+        */
     }
 }
